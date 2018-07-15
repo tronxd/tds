76c561f6269331243e589508bb33a176d9d5328c
Hadar <hadarskipper@gmail.com>
2018-07-15 21:02:37 +0300

    added file names for analasys

---
+++ get_spec_plots.py
@@ -0,0 +1,3 @@
+normal = ['CELL_NORM_2', 'CELL_NORM_3', 'CELL_NORM_4']
+anomaly = ['CELL_CW_-20MHz_0dB', 'CELL_CW_-20MHz_10dB', 'CELL_SWP_18MHz_50us_0dB', \
+          'CELL_SWP_18MHz_50us_10dB', 'CELL_SWP_18MHz_100us_0dB', 'CELL_SWP_18MHz_100us_10dB']
