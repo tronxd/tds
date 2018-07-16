c451ba180ef2f052a4d98ae7b652466c6f93168e
Naor <tron47@gmail.com>
2018-07-17 00:51:06 +0300

    visu

--- base_deep/ae_deep_model.py
+++ base_deep/ae_deep_model.py
@@ -18,7 +18,7 @@ class AeDeepModel(BaseDeepModel):
 
     def build_model(self,input_shape,opt=None,loss_fn=None):
         self.input_shape = input_shape
-        self.model = get_conv_autoencoder_model(input_shape)
+        self.model = AeDeepModel.get_conv_autoencoder_model(input_shape)
         if opt:
             if self.gpus <= 1:
                 self.model.summary()
@@ -33,56 +33,56 @@ class AeDeepModel(BaseDeepModel):
                     # from keras.utils import plot_model
                     # plot_model(self.model, 'model/ae/model_arch.png', show_layer_names=False, show_shapes=True)
 
+    @staticmethod
+    def get_conv_autoencoder_model(input_shape):
+        height = input_shape[0]
+        width = input_shape[1]
+        encoding_dimesions = []
+        kernel_shape = (height // 10) , (width // 10)
+        inputs = Input(shape=input_shape,name='input')
+        encoding_dimesions.append(get_layer_height_width(inputs))
 
-def get_conv_autoencoder_model(input_shape):
-    height = input_shape[0]
-    width = input_shape[1]
-    encoding_dimesions = []
-    kernel_shape = (height // 10) , (width // 10)
-    inputs = Input(shape=input_shape,name='input')
-    encoding_dimesions.append(get_layer_height_width(inputs))
+        x = Conv2D(4, kernel_shape,padding='same',strides=2,name='conv1')(inputs)
+        x = LeakyReLU(alpha=1e-1)(x)
+        encoding_dimesions.append(get_layer_height_width(x))
 
-    x = Conv2D(4, kernel_shape,padding='same',strides=2,name='conv1')(inputs)
-    x = LeakyReLU(alpha=1e-1)(x)
-    encoding_dimesions.append(get_layer_height_width(x))
+        encoded = Conv2D(2, kernel_shape,padding='same',strides=2,name='conv2')(x)
+        encoded = LeakyReLU(alpha=1e-1)(encoded)
 
-    encoded = Conv2D(2, kernel_shape,padding='same',strides=2,name='conv2')(x)
-    encoded = LeakyReLU(alpha=1e-1)(encoded)
+        x = Conv2DTranspose(2, kernel_shape , strides=2,padding='same',name='deconv1')(encoded)
+        x = LeakyReLU(alpha=1e-1)(x)
+        x = get_crop_layer(x , encoding_dimesions.pop())(x)
 
-    x = Conv2DTranspose(2, kernel_shape , strides=2,padding='same',name='deconv1')(encoded)
-    x = LeakyReLU(alpha=1e-1)(x)
-    x = get_crop_layer(x , encoding_dimesions.pop())(x)
+        x = Conv2DTranspose(4, kernel_shape,strides=2,padding='same',name='deconv2')(x)
+        x = LeakyReLU(alpha=1e-1)(x)
+        x = get_crop_layer(x , encoding_dimesions.pop())(x)
+        decoded = Conv2D(1 , kernel_shape,activation='linear',padding='same',name='conv5')(x)
 
-    x = Conv2DTranspose(4, kernel_shape,strides=2,padding='same',name='deconv2')(x)
-    x = LeakyReLU(alpha=1e-1)(x)
-    x = get_crop_layer(x , encoding_dimesions.pop())(x)
-    decoded = Conv2D(1 , kernel_shape,activation='linear',padding='same',name='conv5')(x)
+        model=Model(inputs,decoded)
+        return model
 
-    model=Model(inputs,decoded)
-    return model
+    @staticmethod
+    def get_conv_autoencoder_model_paper(input_shape):
+        num_features=input_shape[0]
+        block_length = input_shape[1]
+        inputs = Input(shape=input_shape,name='input')
+        conv1 = Conv2D(1, (num_features , int(block_length / 2) - 4 ), activation='linear', padding='same')(inputs)
 
+        conv1_flat = Flatten()(conv1)
+    #      h1 = Dense((int(block_length / 2)), activation=K.hard_sigmoid ,
+    #                 activity_regularizer=regularizers.l1(10e-5) ,
+    #                 kernel_regularizer=regularizers.l2(0.5))(conv1_flat)
 
-def get_conv_autoencoder_model_paper(input_shape):
-    num_features=input_shape[0]
-    block_length = input_shape[1]
-    inputs = Input(shape=input_shape,name='input')
-    conv1 = Conv2D(1, (num_features , int(block_length / 2) - 4 ), activation='linear', padding='same')(inputs)
+        h1=Dense(int(block_length) ,activation=K.sigmoid , activity_regularizer=regularizers.l1(0) , name='hidden1')(conv1_flat)
+    #     h2 = Dense(num_features * block_length , activation=K.hard_sigmoid ,
+    #                 activity_regularizer=regularizers.l1(10e-5) ,
+    #                 kernel_regularizer=regularizers.l2(0.1))(h1)
 
-    conv1_flat = Flatten()(conv1)
-#      h1 = Dense((int(block_length / 2)), activation=K.hard_sigmoid ,
-#                 activity_regularizer=regularizers.l1(10e-5) ,
-#                 kernel_regularizer=regularizers.l2(0.5))(conv1_flat)
+        h2=Dense(block_length*num_features , activation=K.sigmoid, activity_regularizer=regularizers.l1(0) , name='hidden2')(h1)
+        h2_reshape = Reshape(input_shape)(h2)
+        outputs = Conv2D(1, ((int(block_length / 2) - 4), 1), activation='linear', padding='same')(h2_reshape)
+        model = Model(inputs=inputs, outputs=outputs)
+        model.compile(optimizer='adam', loss='mse')
 
-    h1=Dense(int(block_length) ,activation=K.sigmoid , activity_regularizer=regularizers.l1(0) , name='hidden1')(conv1_flat)
-#     h2 = Dense(num_features * block_length , activation=K.hard_sigmoid ,
-#                 activity_regularizer=regularizers.l1(10e-5) ,
-#                 kernel_regularizer=regularizers.l2(0.1))(h1)
-
-    h2=Dense(block_length*num_features , activation=K.sigmoid, activity_regularizer=regularizers.l1(0) , name='hidden2')(h1)
-    h2_reshape = Reshape(input_shape)(h2)
-    outputs = Conv2D(1, ((int(block_length / 2) - 4), 1), activation='linear', padding='same')(h2_reshape)
-    model = Model(inputs=inputs, outputs=outputs)
-    model.compile(optimizer='adam', loss='mse')
-
-    model.summary()
-    return model
+        model.summary()
+        return model
