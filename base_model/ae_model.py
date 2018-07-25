4d6f8f53fc6c0f5f297bb993ef80cb4eedd14978
Hadar <hadarskipper@gmail.com>
2018-07-25 13:36:55 +0300

    ae model merged!

--- base_model/ae_model.py
+++ base_model/ae_model.py
@@ -1,13 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from base_model.base_model_class import BaseModel
+from base_model.base_net_model import BaseNetModel
 
 import os
 from utilities.preprocessing import  iq2fft, scale_train_vectors, whiten_train_data, get_config, reshape_to_blocks, add_noise, persist_val_stat, load_object, whiten_test_data, scale_test_vectors, load_val_stat, get_basic_block_len
 from utilities.learning import split_train_validation, train_model, predict_ae_error_vectors
 from base_deep.ae_deep_model import AeDeepModel
+from utilities.learning import get_layer_height_width, get_crop_layer
 from keras.optimizers import Adam
+from keras import Input, Model, backend as K, regularizers
+from keras.layers import Conv2D, LeakyReLU, Conv2DTranspose, Flatten, Dense, Reshape
+from keras.utils.training_utils import multi_gpu_model
+
+import tensorflow as tf
+
+
 
 
 conf=get_config()
@@ -15,7 +23,7 @@ lr=conf['learning']['ae']['lr']
 basic_time = conf['preprocessing']['basic_time']
 rbw = conf['preprocessing']['ae']['rbw']
 use_whitening=conf['preprocessing']['use_whitening']
-use_scaling = conf['preprocessing']['use_scaling']
+use_scaling = conf['preprocessing']['ae']['use_scaling']
 feature_range = conf['preprocessing']['feature_range']
 sigma_ae = conf['detection']['ae']['sigma']
 block_shape = conf['learning']['ae']['block_shape']
@@ -30,12 +38,13 @@ loss_fn = 'mse'
 
 
 
-class AeModel(BaseModel):
+class AeModel(BaseNetModel):
     def __init__(self, model_path=None):
+        super(AeModel, self).__init__(train_params, gpus)
         self.rbw = rbw
         self.name = 'ae'
         if not model_path:
-            self.model_path = os.path.join('model',self.name+'_' + str(int(self.rbw)))
+            self.model_path = os.path.join('model',self.name+'_' + str(int(self.rbw))+'_block_{}X{}'.format(block_shape[0], block_shape[1]))
         else:
             self.model_path = model_path
         if not os.path.exists(self.model_path):
@@ -44,9 +53,8 @@ class AeModel(BaseModel):
         self.loaded = False
         self.whiten_path = None
         self.scaler = None
-        self.conv_model = None
 
-    def preprocess_train(self, iq_data, sample_rate):
+    def preprocess_train_data(self, iq_data, sample_rate, rbw=None):
         scaler_path = os.path.join(self.model_path, "train_scaler.pkl")
         self.whiten_path = os.path.join(self.model_path, "zca_scaler.pkl")
 
@@ -61,21 +69,41 @@ class AeModel(BaseModel):
         if use_scaling:
             (fft_d, scaler) = scale_train_vectors(fft_d, scaler_path, rng=feature_range)
             self.scaler = scaler
+        return ((freqs, time), fft_d)
 
+    def preprocess_test_data(self, iq_data, sample_rate, rbw=None):
+        self.whiten_path = os.path.join(self.model_path, "zca_scaler.pkl")
+
+        ## whitening
+        if use_whitening:
+            iq_data = whiten_test_data(iq_data, self.whiten_path)
+
+        ## getting spectrogram
+        freqs, time, fft_d = iq2fft(iq_data, sample_rate, self.rbw)
+
+        ## scaling spectrogram
+        if use_scaling:
+            fft_d = scale_test_vectors(fft_d, scaler=self.scaler)
+        return ((freqs, time), fft_d)
+
+
+    def train_data(self, preprocessed_data):
         ## traing auto encoder
-        self.conv_model = AeDeepModel(train_params, self.model_path, gpus)
+        fft_d = preprocessed_data
         block_indices, X_train = reshape_to_blocks(fft_d, block_shape)
         (X_train, _, X_val, _) = split_train_validation(X_train, X_train, validation_split)
-        self.conv_model.build_model(X_train.shape[1:], opt, loss_fn)
+
+        deep_model_input_shape = (block_shape[0], block_shape[1], 1)
+        self.build_model(deep_model_input_shape, opt, loss_fn)
         if use_noise:
             X_train_noisy = add_noise(X_train)
-            train_model(self.conv_model, X_train_noisy, X_train, X_val, X_val, train_params)
+            train_model(self, X_train_noisy, X_train, X_val, X_val, train_params)
         else:
-            train_model(self.conv_model, X_train, X_train, X_val, X_val, train_params)
+            train_model(self, X_train, X_train, X_val, X_val, train_params)
 
-        train_errors = predict_ae_error_vectors(X_train, X_train, self.conv_model, batch_size)
-        val_errors = predict_ae_error_vectors(X_val, X_val, self.conv_model, batch_size)
-        persist_val_stat(val_errors, self.conv_model.weights_path)
+        train_errors = predict_ae_error_vectors(X_train, X_train, self, batch_size)
+        val_errors = predict_ae_error_vectors(X_val, X_val, self, batch_size)
+        persist_val_stat(val_errors, self.model_path)
         self.loaded = True
 
 
@@ -117,20 +145,12 @@ class AeModel(BaseModel):
         if not self.loaded:
             self.load_model()
 
-        ## whitening
-        if use_whitening:
-            iq_data_basic_block = whiten_test_data(iq_data_basic_block, self.whiten_path)
-
-        ## getting spectrogram
-        freqs, time, fft_d = iq2fft(iq_data_basic_block, sample_rate, self.rbw)
-
-        ## scaling spectrogram
-        if use_scaling:
-            fft_d = scale_test_vectors(fft_d, self.scaler)
+        f_t, fft_d = self.preprocess_test_data(iq_data_basic_block, sample_rate)
+        freqs, time = f_t
 
         ## predicts of auto encoder
         block_indices, data_blocks = reshape_to_blocks(fft_d, block_shape)
-        data_ae_errors = predict_ae_error_vectors(data_blocks, data_blocks, self.conv_model, batch_size)
+        data_ae_errors = predict_ae_error_vectors(data_blocks, data_blocks, self, batch_size)
         data_ae_errors = np.reshape(data_ae_errors, (block_indices.shape[0], block_indices.shape[1]))
 
         ## getting erros per block
@@ -148,8 +168,54 @@ class AeModel(BaseModel):
         if use_whitening:
             self.whiten_path = os.path.join(self.model_path, "zca_scaler.pkl")
         self.scaler = load_object(os.path.join(self.model_path, 'train_scaler.pkl'))
-        self.conv_model = AeDeepModel(train_params, self.model_path, gpus)
+
         deep_model_input_shape = (block_shape[0], block_shape[1], 1)
-        self.conv_model.build_model(deep_model_input_shape, opt, loss_fn)
-        self.conv_model.load_weights()
+        self.build_model(deep_model_input_shape, opt, loss_fn)
+        self.load_weights()
+
         self.loaded = True
+
+    def build_model(self,input_shape, opt=None, loss_fn=None):
+        self.input_shape = input_shape
+        self.net_model = AeDeepModel.get_conv_autoencoder_model(input_shape)
+        if opt:
+            if self.gpus <= 1:
+                self.net_model.summary()
+                self.net_model.compile(optimizer=opt, loss=loss_fn)
+                # from keras.utils import plot_model
+                # plot_model(self.model, 'model/ae/model_arch.png',show_layer_names=False, show_shapes=True)
+
+            else:
+                with tf.device("/cpu:0"):
+                    self.gpu_model = multi_gpu_model(self.net_model, gpus=self.gpus)
+                    self.gpu_model.compile(optimizer=opt,loss=loss_fn)
+                    # from keras.utils import plot_model
+                    # plot_model(self.model, 'model/ae/model_arch.png', show_layer_names=False, show_shapes=True)
+
+    @staticmethod
+    def get_conv_autoencoder_model(input_shape):
+        height = input_shape[0]
+        width = input_shape[1]
+        encoding_dimesions = []
+        kernel_shape = (height // 10) , (width // 10)
+        inputs = Input(shape=input_shape,name='input')
+        encoding_dimesions.append(get_layer_height_width(inputs))
+
+        x = Conv2D(4, kernel_shape,padding='same',strides=2,name='conv1')(inputs)
+        x = LeakyReLU(alpha=1e-1)(x)
+        encoding_dimesions.append(get_layer_height_width(x))
+
+        encoded = Conv2D(2, kernel_shape,padding='same',strides=2,name='conv2')(x)
+        encoded = LeakyReLU(alpha=1e-1)(encoded)
+
+        x = Conv2DTranspose(2, kernel_shape , strides=2,padding='same',name='deconv1')(encoded)
+        x = LeakyReLU(alpha=1e-1)(x)
+        x = get_crop_layer(x , encoding_dimesions.pop())(x)
+
+        x = Conv2DTranspose(4, kernel_shape,strides=2,padding='same',name='deconv2')(x)
+        x = LeakyReLU(alpha=1e-1)(x)
+        x = get_crop_layer(x , encoding_dimesions.pop())(x)
+        decoded = Conv2D(1 , kernel_shape,activation='linear',padding='same',name='conv5')(x)
+
+        model=Model(inputs,decoded)
+        return model
